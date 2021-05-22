7649056c78d279adaef17385720c6489573ab87c
anassohail99 <anas.sohailabs1@gmail.com>
2021-05-22 21:02:41 +0500

    updated

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,40 +1,40 @@
-import React, { useState, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import { Paper, InputBase, IconButton, useMediaQuery } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
-import { validateAccountAddress } from './../../utils/validators/validators';
-import axios from 'axios';
-import { UserSnapshotContext } from './../../context/UserSnapshot';
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper, InputBase, IconButton, useMediaQuery } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import { validateAccountAddress } from "./../../utils/validators/validators";
+import axios from "axios";
+import { UserSnapshotContext } from "./../../context/UserSnapshot";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    padding: '4px 8px',
-    display: 'flex',
-    alignItems: 'center',
-    width: '40%',
-    backgroundColor: '#000000',
+    padding: "4px 8px",
+    display: "flex",
+    alignItems: "center",
+    width: "40%",
+    backgroundColor: "#000000",
   },
   rootMobile: {
-    padding: '4px 8px',
-    display: 'flex',
-    alignItems: 'center',
-    width: '90%',
-    backgroundColor: '#000000',
+    padding: "4px 8px",
+    display: "flex",
+    alignItems: "center",
+    width: "90%",
+    backgroundColor: "#000000",
   },
   input: {
     marginLeft: theme.spacing(2),
     flex: 1,
-    color: '#ffffff',
-    fontSize: '1.2rem'
+    color: "#ffffff",
+    fontSize: "1.2rem",
   },
   iconButton: {
     padding: 10,
-    color: '#696969',
+    color: "#696969",
   },
   iconButtonDisabled: {
     padding: 10,
-    color: "#69696980 !important"
+    color: "#69696980 !important",
   },
   // divider: {
   //   height: 28,
@@ -45,12 +45,11 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function SearchBar() {
-
   const { updateSnapshot } = useContext(UserSnapshotContext);
 
   const classes = useStyles();
   const history = useHistory();
-  const tablet = useMediaQuery('(min-width:768px)')
+  const tablet = useMediaQuery("(min-width:768px)");
 
   const [accountAddress, setAccountAddress] = useState();
   const [validAccountAddress, setValidAccountAddress] = useState(false);
@@ -72,16 +71,16 @@ function SearchBar() {
 
     try {
       const res = await axios({
-        method: 'GET',
-        url: `https://server2.flashstake.io/aqua/${accountAddress}`
-      })
+        method: "GET",
+        url: `https://server2.flashstake.io/aqua/${accountAddress}`,
+      });
 
       if (res.status === 200) {
-        console.log(res);
+        // console.log(res);
         updateSnapshot(res?.data);
         history.push(`/account/${accountAddress}`);
       } else {
-        console.log('Could not find the account');
+        console.log("Could not find the account");
       }
     } catch (error) {
       console.log(error);
@@ -89,15 +88,24 @@ function SearchBar() {
   }
 
   return (
-    <Paper elevation={3} component="form" className={tablet ? classes.root : classes.rootMobile}>
+    <Paper
+      elevation={3}
+      component="form"
+      className={tablet ? classes.root : classes.rootMobile}
+    >
       <InputBase
         className={classes.input}
         placeholder="Enter Account Address"
-        inputProps={{ 'aria-label': 'enter account address' }}
+        inputProps={{ "aria-label": "enter account address" }}
         onChange={(event) => handleInput(event)}
         value={accountAddress}
       />
-      <IconButton type="submit" className={!validAccountAddress ? classes.iconButtonDisabled : classes.iconButton} aria-label="search"
+      <IconButton
+        type="submit"
+        className={
+          !validAccountAddress ? classes.iconButtonDisabled : classes.iconButton
+        }
+        aria-label="search"
         disabled={!validAccountAddress}
         onClick={(event) => handleSubmit(event)}
       >
--- src/components/SnapshotTable/index.js
+++ src/components/SnapshotTable/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState, useEffect, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 import {
   Paper,
   Table,
@@ -10,75 +10,75 @@ import {
   TableHead,
   TablePagination,
   TableRow,
-} from '@material-ui/core';
-import { format } from 'date-fns';
-import BigNumber from 'bignumber.js';
+} from "@material-ui/core";
+import { format } from "date-fns";
+import BigNumber from "bignumber.js";
 
-import web3 from 'web3';
-import { UserSnapshotContext } from './../../context/UserSnapshot';
+import web3 from "web3";
+import { UserSnapshotContext } from "./../../context/UserSnapshot";
 
 const columns = [
-  { id: 'blockNumber', label: 'Block Number', minWidth: 100, align: 'center' },
-  { id: 'day', label: 'Day', minWidth: 170, align: 'center' },
+  { id: "blockNumber", label: "Block Number", minWidth: 100, align: "center" },
+  { id: "day", label: "Day", minWidth: 170, align: "center" },
   {
-    id: 'xioBalance',
-    label: 'XIO Balance',
+    id: "xioBalance",
+    label: "XIO Balance",
     minWidth: 170,
-    align: 'center',
-    format: value => value.toLocaleString('en-US'),
+    align: "center",
+    format: (value) => value.toLocaleString("en-US"),
   },
   {
-    id: 'xioAppBalance',
-    label: 'Balance in Flash protocol',
+    id: "xioAppBalance",
+    label: "Balance in Flash protocol",
     minWidth: 170,
-    align: 'center',
-    format: value => value.toLocaleString('en-US'),
+    align: "center",
+    format: (value) => value.toLocaleString("en-US"),
   },
   {
-    id: 'xioAppBalance2',
-    label: 'Balance in Flash protocol v2',
+    id: "xioAppBalance2",
+    label: "Balance in Flash protocol v2",
     minWidth: 170,
-    align: 'center',
+    align: "center",
   },
   {
-    id: 'uniBalance',
-    label: 'Balance in Uniswap',
+    id: "uniBalance",
+    label: "Balance in Uniswap",
     minWidth: 170,
-    align: 'center',
-    format: value => value.toFixed(2),
+    align: "center",
+    format: (value) => value.toFixed(2),
   },
   {
-    id: 'aquaPrice',
-    label: 'Aqua per day',
+    id: "aquaPrice",
+    label: "Aqua per day",
     minWidth: 170,
-    align: 'center',
+    align: "center",
   },
 ];
 
 const useStyles = makeStyles({
   root: {
-    width: '100%',
+    width: "100%",
   },
   container: {
     maxHeight: 640,
     // marginTop: 150,
   },
   tableHeadCell: {
-    backgroundColor: '#000000',
-    color: '#D89C74',
-    border: 'none',
+    backgroundColor: "#000000",
+    color: "#D89C74",
+    border: "none",
   },
   tableBodyCell: {
-    backgroundColor: '#171717',
-    color: '#ffffff',
-    border: 'none',
+    backgroundColor: "#171717",
+    color: "#ffffff",
+    border: "none",
   },
   tablePagination: {
-    backgroundColor: '#171717',
-    color: '#ffffff',
-    fill: '#ffffff',
-    '& svg.MuiSelect-icon': {
-      color: '#fff',
+    backgroundColor: "#171717",
+    color: "#ffffff",
+    fill: "#ffffff",
+    "& svg.MuiSelect-icon": {
+      color: "#fff",
     },
   },
 });
@@ -100,7 +100,7 @@ export default function StickyHeadTable() {
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = event => {
+  const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
@@ -109,7 +109,7 @@ export default function StickyHeadTable() {
     (async () => {
       if (userSnapshot) {
         let snapshots = [...userSnapshot?.data?.balances];
-        console.log(snapshots);
+        // console.log(snapshots);
         const newSnapshots = formatSnapshotBalance(snapshots);
 
         setRows(sortSnapshot(newSnapshots));
@@ -124,13 +124,21 @@ export default function StickyHeadTable() {
   }
 
   function formatSnapshotBalance(snapshots) {
-    return snapshots.map(snapshot => {
+    return snapshots.map((snapshot) => {
       return {
         ...snapshot,
-        xioBalance: parseFloat(web3.utils.fromWei(snapshot.xioBalance)).toFixed(2),
-        xioAppBalance: parseFloat(web3.utils.fromWei(snapshot.xioAppBalance)).toFixed(2),
-        uniBalance: parseFloat(web3.utils.fromWei(snapshot.uniBalance)).toFixed(2),
-        xioAppBalance2: parseFloat(web3.utils.fromWei(snapshot.xioAppBalance2)).toFixed(2),
+        xioBalance: parseFloat(web3.utils.fromWei(snapshot.xioBalance)).toFixed(
+          2
+        ),
+        xioAppBalance: parseFloat(
+          web3.utils.fromWei(snapshot.xioAppBalance)
+        ).toFixed(2),
+        uniBalance: parseFloat(web3.utils.fromWei(snapshot.uniBalance)).toFixed(
+          2
+        ),
+        xioAppBalance2: parseFloat(
+          web3.utils.fromWei(snapshot.xioAppBalance2)
+        ).toFixed(2),
       };
     });
   }
@@ -167,19 +175,23 @@ export default function StickyHeadTable() {
     const xioAppPricev2 = xioApp.multipliedBy(xioAppBalance2).dividedBy(100);
     const xioPrice = xioApp.multipliedBy(xioBalance).dividedBy(100);
 
-    const aquaPrice = uniswapPrice.plus(xioAppPrice).plus(xioAppPricev2).plus(xioPrice).toString();
+    const aquaPrice = uniswapPrice
+      .plus(xioAppPrice)
+      .plus(xioAppPricev2)
+      .plus(xioPrice)
+      .toString();
     ap = new BigNumber(ap).plus(aquaPrice);
-    console.log(ap.toString());
+    // console.log(ap.toString());
     return aquaPrice;
   };
 
   return (
     <Paper className={classes.root}>
       <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label='sticky table'>
+        <Table stickyHeader aria-label="sticky table">
           <TableHead className={classes.tableHead}>
             <TableRow>
-              {columns.map(column => (
+              {columns.map((column) => (
                 <TableCell
                   key={column.id}
                   align={column.align}
@@ -193,10 +205,15 @@ export default function StickyHeadTable() {
           </TableHead>
           <TableBody>
             {/* slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) */}
-            {rows.map(row => {
+            {rows.map((row) => {
               return (
-                <TableRow hover role='checkbox' tabIndex={-1} key={row.timestamp}>
-                  {columns.map(column => {
+                <TableRow
+                  hover
+                  role="checkbox"
+                  tabIndex={-1}
+                  key={row.timestamp}
+                >
+                  {columns.map((column) => {
                     const value = row[column.id];
                     return (
                       <TableCell
@@ -204,13 +221,19 @@ export default function StickyHeadTable() {
                         align={column.align}
                         className={classes.tableBodyCell}
                       >
-                        {column.format && typeof value === 'number'
+                        {column.format && typeof value === "number"
                           ? column.format(value)
-                          : column.id === 'day'
-                          ? row['blockNumber'] === 12149961
-                            ? `${value}-${format(new Date(1617115051 * 1000), 'MMM')}`
-                            : `${value}-${format(new Date(row['timestamp'] * 1000), 'MMM')}`
-                          : column.id === 'aquaPrice'
+                          : column.id === "day"
+                          ? row["blockNumber"] === 12149961
+                            ? `${value}-${format(
+                                new Date(1617115051 * 1000),
+                                "MMM"
+                              )}`
+                            : `${value}-${format(
+                                new Date(row["timestamp"] * 1000),
+                                "MMM"
+                              )}`
+                          : column.id === "aquaPrice"
                           ? findAquaPrice(row)
                           : value}
                       </TableCell>
@@ -224,7 +247,7 @@ export default function StickyHeadTable() {
       </TableContainer>
       <TablePagination
         rowsPerPageOptions={[10, 25, 100]}
-        component='div'
+        component="div"
         count={rows.length}
         rowsPerPage={rowsPerPage}
         page={page}
